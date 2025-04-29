b11ac942c48eca6e6c269ec87c00b546619353b3
Darshan Heble <darshanheble@gmail.com>
2025-04-30 01:21:15 +0530

    Added privy for wallet connect and social login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "@components/Sidebar";
 import Header from "@components/Header";
 import DashBoard from "@pages/DashBoard";
-import Login from "@pages/Login";
+// import Login from "@pages/Login";
 import Landing from "@pages/Landing";
 import VideoPlayerPage from "@pages/VideoPlayerPage";
 import "./index.css";
@@ -10,16 +10,16 @@ import "./index.css";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <div className="flex h-screen">
-        {/* Sidebar */}
         <Sidebar />
 
         {/* Main Content */}
         <div className="flex-1">
           <Routes>
             <Route path="/" element={<Landing />} />
-            <Route path="/header" element ={<Header />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/header" element={<Header />} />
+            {/* <Route path="/login" element={<Login />} /> */}
             <Route path="/dashboard" element={<DashBoard />} />
             <Route path="/video" element={<VideoPlayerPage />} />
           </Routes>
