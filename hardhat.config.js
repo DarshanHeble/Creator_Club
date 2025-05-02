e60fe584754fd2ae70b9f92ed7db72cce494955e
faizan1317 <faizandeshmukh13@gmail.com>
2025-05-02 23:14:47 +0530

    Verify token balance after deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     soneium: {
-      url: "https://soneium-minato.rpc.scs.startale.com/?apikey=nOuphkfjI6q6fDc6G0uD0oGHMlKUH8x",
-      accounts: ["f84430dd5c00cf608dc653be79f894731781553fda04e19c97f1a56e45795966"], // Replace with your MetaMask test wallet private key
+      url: "https://rpc.minato.soneium.org", // Temporary public RPC
+      accounts: ["f84430dd5c00cf608dc653be79f894731781553fda04e19c97f1a56e45795966"],
       chainId: 1946,
     },
   },
