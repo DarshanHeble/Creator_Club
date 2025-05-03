c8db90a5aa59ed5d3b4982b31642a6d268ce743a
Darshan Heble <darshanheble@gmail.com>
2025-05-03 17:17:28 +0530

    ..

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,46 +1,33 @@
 import { usePrivy, useLogin, useLogout } from "@privy-io/react-auth";
+import { userService } from "@services/userService";
 import { useNavigate } from "react-router-dom";
 
 export const useAuth = () => {
-  const { user, ready, authenticated } = usePrivy();
+  const { user: privyUser, ready, authenticated } = usePrivy();
+  const navigate = useNavigate();
+
   const { login } = useLogin({
     onComplete() {
-      if (authenticated) {
-        navigate(`/user/${user?.id}/dashboard`);
+      if (ready && authenticated && privyUser) {
+        navigate(`/user/${privyUser?.id}/dashboard`);
+
+        userService.createUser({
+          id: privyUser.id,
+          walletAddress: privyUser.wallet?.address || "",
+          role: "fan",
+        });
       }
     },
   });
+
   const { logout } = useLogout({
     onSuccess() {
       navigate("/");
     },
   });
-  const navigate = useNavigate();
-
-  //   const loginWithRedirect = useCallback(async () => {
-  //     try {
-  //       await login();
-  //       // After successful login, user will be redirected to dashboard
-  //       if (user?.id) {
-  //         navigate(`/user/${user.id}/dashboard`);
-  //       }
-  //     } catch (error) {
-  //       console.error("Login failed:", error);
-  //     }
-  //   }, [login, navigate, user?.id]);
-
-  //   const logout = useCallback(async () => {
-  //     try {
-  //       await privyLogout();
-  //       // After logout, redirect to landing page
-  //       navigate("/");
-  //     } catch (error) {
-  //       console.error("Logout failed:", error);
-  //     }
-  //   }, [privyLogout, navigate]);
 
   return {
-    user,
+    privyUser,
     ready,
     authenticated,
     login,
